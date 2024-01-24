3b706c181c0ee4e4088f7289cb514a834edfc30d
emmett <emmett@christensons.net>
2024-01-23 22:45:10 -0600

    restructered code

--- Main/sw.js
+++ Main/sw.js
@@ -1,6 +1,6 @@
 "mode strict";
 
-var cache = caches.open("v1");
+var cache = caches.open("v1.1");
 
 const addResourcesToCache = async (resources) => {
     cache.then(c => c.addAll(resources))
@@ -8,22 +8,22 @@ const addResourcesToCache = async (resources) => {
 
 self.addEventListener("install", (event) => {
     event.waitUntil(
-	addResourcesToCache([
-	    "./",
-	    "./index.html",
-	    "./qrcode.js",
-	    "./minutebots-logo.png",
-		"./minutebots-logo.ico"
-	]).then(() => {
-	    skipWaiting();
-	})
+		addResourcesToCache([
+			"./",
+			"./index.html",
+			"./qrcode.js",
+			"./minutebots-logo.png",
+			"./minutebots-logo.ico"
+		]).then(() => {
+			skipWaiting();
+		})
     );
-    // console.log('installed', new Date());
+    // console.log('installed', new Data());
     return event
 });
 
 self.addEventListener("activate", (event) => {
-    // console.log('activated', new Date());
+    // console.log('activated', new Data());
     clients.claim();
     return event
 });
@@ -32,17 +32,17 @@ self.addEventListener("activate", (event) => {
 self.addEventListener("fetch", (event) => {
     // console.log(event.request.url);
     event.respondWith(
-	(async () => {
-	    let c = await cache;
-	    let r = await c.match(event.request);
-	    if (r) {
-		// console.log("found in cache");
-		// console.log(r);
-		return r;
-	    } else {
-		// console.log("fetch", event.request, "not cached");
-		return fetch(event.request);
-	    }
-	})()
+		(async () => {
+			let c = await cache;
+			let r = await c.match(event.request);
+			if (r) {
+			// console.log("found in cache");
+			// console.log(r);
+			return r;
+			} else {
+			// console.log("fetch", event.request, "not cached");
+			return fetch(event.request);
+			}
+		})()
     )
 });
