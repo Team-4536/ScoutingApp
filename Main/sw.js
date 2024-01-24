2318f1268d7a30c98ea34cf4a8b4293a4ddc8f72
emmett <emmett@christensons.net>
2024-01-23 21:49:55 -0600

    added the code

---
+++ Main/sw.js
@@ -0,0 +1,48 @@
+"mode strict";
+
+var cache = caches.open("v1");
+
+const addResourcesToCache = async (resources) => {
+    cache.then(c => c.addAll(resources))
+};
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+	addResourcesToCache([
+	    "./",
+	    "./index.html",
+	    "./qrcode.js",
+	    "./minutebots-logo.png",
+		"./minutebots-logo.ico"
+	]).then(() => {
+	    skipWaiting();
+	})
+    );
+    // console.log('installed', new Date());
+    return event
+});
+
+self.addEventListener("activate", (event) => {
+    // console.log('activated', new Date());
+    clients.claim();
+    return event
+});
+
+
+self.addEventListener("fetch", (event) => {
+    // console.log(event.request.url);
+    event.respondWith(
+	(async () => {
+	    let c = await cache;
+	    let r = await c.match(event.request);
+	    if (r) {
+		// console.log("found in cache");
+		// console.log(r);
+		return r;
+	    } else {
+		// console.log("fetch", event.request, "not cached");
+		return fetch(event.request);
+	    }
+	})()
+    )
+});
