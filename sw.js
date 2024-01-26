7cd1ac4e0db54bd0b0b18fdec9785a650afe511a
emmett <emmett@christensons.net>
2024-01-25 19:22:23 -0600

    broke the code, then fixed it (:

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 "mode strict";
 
-var cache = caches.open("v5");
+var cache = caches.open("v3.14159265");
 
 const addResourcesToCache = async (resources) => {
     cache.then(c => c.addAll(resources))
