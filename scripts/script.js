3e96d61b5691d5d372d8fc660b9d6bc7584aad92
emmett <emmett@christensons.net>
2024-01-30 17:30:31 -0600

    g

--- scripts/script.js
+++ scripts/script.js
@@ -128,7 +128,11 @@ async function fillTeamData(teamData) {
   }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener(input, textfield), function() {
+    dataObject()
+  }
+
   document.getElementById('a-stop').addEventListener('change', function() {
     aStop();
   });
