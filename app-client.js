e76400abf71472d8f949d844d4702d7af9d0cd7d
emmett <emmett@christensons.net>
2024-03-02 11:03:56 -0600

    going to revert app-cliernt

--- app-client.js
+++ app-client.js
@@ -30,9 +30,11 @@ const call = async (method, ...args) => {
 
 navigator.serviceWorker.addEventListener("message", (m) => {
     let reply = m.data.reply;
+    console.log('request id', reply.requestId)
     let rq = pendingRequests[reply.requestId];
     if (!rq) {
-        console.log(`received unexpected reply for ${reply.requestid}`);
+        console.log('message', m)
+        console.log(`received unexpected reply for ${reply.requestId}`);
     } else {
         rq.resolve(reply.result);
         clearTimeout(rq.timer);
