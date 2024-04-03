e8fd64cf9e46eba28f83dcc2b2a6b30e9f607947
Mark Christenson <mark@christensons.net>
2024-04-03 17:30:56 -0500

    use a password instead of the raw key

--- scripts/qr-scan-new.js
+++ scripts/qr-scan-new.js
@@ -16,6 +16,71 @@ Object.assign(Core.CoreModule.engineResourcePaths, {
     utility: "/assets/scan/"
 })
 
+const licenseEncrypted = new Uint8Array([
+    16,12,219,45,110,161,53,182,210,149,24,16,77,134,7,189,113,207,22,148,86,111,47,201,
+    70,233,153,184,54,165,146,209,188,25,1,53,53,24,32,94,75,250,106,104,224,135,72,83,78,
+    210,79,69,166,206,144,114,161,148,53,3,120,0,106,24,115,40,161,41,44,6,122,144,42,71,
+    150,25,243,61,71,101,160,109,35,104,235,185,113,88,237,120,228,74,153,151,181,140,23,
+    90,85,94,216,158,73,78,234,130,3,252,159,4,105,113,248,202,19,132,211,31,95,18,30,
+    116,198,195,193,255,127,239,70,184,138,47,190,92,3,249,60,235,124,24,34,63,39,77,
+    6,169,100,146,215,196,225,245,130,245,92,181,244,53,244,154,86,240,145,65,191,252,
+    25,110,15,128,59,87,110,150,243,130,1,135,59,28,235,35,221,64,185,16,112,69,158,248,
+    198,46,57,201,120,159,96,195,97,116,37,89,236,200,170,220,232,10,251,40,248,184,80,
+    53,20,202,187,74,175,238,160,85,196,116,19,41,19,122,34,181,84,133,202,72,204,237,
+    105,104,151,210,144,76,242,73,15,88,237,159,52,38,143,112,178,116,214,137,237,55,49,
+    173,206,193,106,179,56,211,63,13,17,6,250,238,210,162,139,47,143,149,80,168,97,198,
+    194,146,232,123,251,49,232,140,34,24,227,93,198,97,115,47]);
+
+function getPW() {
+    let password = prompt("Scanner password");
+    let enc = new TextEncoder();
+    return crypto.subtle.importKey(
+        "raw",
+        enc.encode(password),
+        {name: "PBKDF2"},
+        false,
+        ["deriveBits", "deriveKey"]
+    );
+}
+
+function getKey(keyMaterial, salt) {
+    return crypto.subtle.deriveKey(
+        {
+            "name": "PBKDF2",
+            salt: salt,
+            "iterations": 100000,
+            "hash": "SHA-256"
+        },
+        keyMaterial,
+        { "name": "AES-GCM", "length": 256},
+        true,
+        [ "encrypt", "decrypt" ]
+    );
+}
+
+async function decryptLicense() {
+    let buffer = licenseEncrypted;
+    let lenS = buffer[0];
+    let lenIV = buffer[1]
+    let s = buffer.slice(2, lenS+2);
+    let iv = buffer.slice(lenS+2, lenS+2+lenIV)
+    let keyMaterial = await getPW();
+    let key = await getKey(keyMaterial, s);
+
+    let ciphertext = buffer.slice(lenS+2+lenIV);
+
+    let decrypted = await crypto.subtle.decrypt(
+        {
+            name: "AES-GCM",
+            iv: iv
+        },
+        key,
+        ciphertext
+    );
+
+    return (new TextDecoder()).decode(decrypted);
+};
+
 const license = async () => {
     const ls = window.localStorage;
 
@@ -23,7 +88,7 @@ const license = async () => {
 
     do {
         if (!key) {
-            key = prompt("Enter scanner license");
+            key = await decryptLicense();
             if (!key) {
                 return false;
             }
